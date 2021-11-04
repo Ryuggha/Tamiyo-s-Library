a8c670cd5db75d8a435542067dce3ed53245471f
Ryuggha <rogerperalesmares493@gmail.com>
2021-11-04 22:12:28 +0100

    gitIgnore

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import discord
 import os
 from discord.ext import commands
 
+inHeroku = False
+
 TOKEN = 'OTA1ODU4ODU1MTQ4MzIyODk4.YYQMzQ.PIR740b8M2LvdSoTG7aBU4PH-uw' ##Testing Token
 
 is_prod = os.environ.get('IS_HEROKU', None)
 if is_prod:
     print('Heroku App is Active')
-    TOKEN = ''
+    inHeroku = True
+
 
 client = commands.Bot(command_prefix='|', description="this is a testing bot")
 
