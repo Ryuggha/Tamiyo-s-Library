be67e89fb7b53ba58c1d4a034a9ddf5da4c38dc5
Ryuggha <rogerperalesmares493@gmail.com>
2021-11-04 23:33:36 +0100

    TrialMessage

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import discord
-import os
 from discord.ext import commands
+from boto.s3.connection import S3Connection
+import os
+import json
 
 inHeroku = False
-
-TOKEN = 'OTA1ODU4ODU1MTQ4MzIyODk4.YYQMzQ.PIR740b8M2LvdSoTG7aBU4PH-uw' ##Testing Token
-
 is_prod = os.environ.get('IS_HEROKU', None)
 if is_prod:
     print('Heroku App is Active')
     inHeroku = True
+
+if inHeroku:
+    TOKEN = os.environ['TOKEN']
+else:
+    localVars = json.load(open('localVars.json', encoding="utf8"))
+    TOKEN = localVars['TOKEN'] ##Testing Token
 
 
 client = commands.Bot(command_prefix='|', description="this is a testing bot")
@@ -20,9 +25,13 @@ async def on_ready():
     print(client.user.name)
     ##print(client.user.id)
     print('------')
+    c = await client.fetch_user(337252465072275456)
+    print(c)
+    await c.send("Test")
 
 @client.command()
 async def ping(ctx):
      await ctx.send('pong')
+
 
 client.run(TOKEN)
