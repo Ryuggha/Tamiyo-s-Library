60648c3e0d313965545dbd4a5551e1a2219a83a8
Ryuggha <rogerperalesmares493@gmail.com>
2022-11-08 06:56:43 +0100

    Tamyo's Remake Init Commit

---
+++ Legacy/README.md
@@ -0,0 +1,1 @@
+# Tamiyo-s-Library
---
+++ build/discordBotMain.js
@@ -0,0 +1,22 @@
+"use strict";
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv/config');
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ]
+});
+client.on('ready', () => {
+    console.log("Logged in as:");
+    console.log(client.user.name);
+    console.log(client.user.id);
+    console.log("---------------");
+});
+client.on('messageCreate', (message) => {
+    if (message.content == 'ping') {
+        message.reply('pong');
+    }
+});
+client.login(process.env.TOKEN);
